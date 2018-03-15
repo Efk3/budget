75d1c2eec582f83455e8b4edd694057d6ab88b28
Krisztián Fekete <en@efk3.net>
2018-03-15 14:12:17 +0100

    feat(auth): add authentication info selector

--- src/app/auth/reducers/auth.selector.ts
+++ src/app/auth/reducers/auth.selector.ts
@@ -1,8 +1,17 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AuthenticationType } from '../models/authentication-type.model';
 import { AuthState } from './auth.state';
 
 export const selectAuthState = createFeatureSelector<AuthState>('auth');
 export const getAccessToken = createSelector(selectAuthState, (state: AuthState) => state.accessToken);
+export const getAuthenticationType = createSelector(selectAuthState, (state: AuthState) => state.type);
 export const getUser = createSelector(selectAuthState, (state: AuthState) => state.user);
 export const getLoggedIn = createSelector(selectAuthState, (state: AuthState) => state.loggedIn);
 export const getFailure = createSelector(selectAuthState, (state: AuthState) => state.error);
+export const getAuthenticationInfo = createSelector(
+  getAccessToken,
+  getAuthenticationType,
+  (accessToken: string, type: AuthenticationType) => {
+    return { accessToken, type };
+  }
+);
